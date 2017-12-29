aad5a5824d6e2eaa177da3844e7d5f04dd14024f
SINOTHK\LiangYT <381518188@qq.com>
2017-12-30 02:19:09 +0800

    2017年12月30日 02:19:05

---
+++ ParentLibrary/src/main/java/com/parent/library/utils/AppUtil.java
@@ -0,0 +1,16 @@
+package com.parent.library.utils;
+
+/**
+ * Created by 梁玉涛 on 2017/12/29.
+ */
+
+public class AppUtil {
+
+
+    public static int[] getScreen() {
+        int[] screenWH = new int[2];
+        screenWH[0] = 11;
+        screenWH[1] = 11;
+        return screenWH;
+    }
+}
--- app/src/main/java/com/parent/library/demo/MainActivity.java
+++ app/src/main/java/com/parent/library/demo/MainActivity.java
@@ -3,11 +3,15 @@ package com.parent.library.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parent.library.utils.AppUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppUtil.getScreen();
     }
 }
