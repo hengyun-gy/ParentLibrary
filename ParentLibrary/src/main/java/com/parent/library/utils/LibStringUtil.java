f5c5225900b6017e17e9ac2e144084cd780839ba
SINOTHK\LiangYT <381518188@qq.com>
2018-01-02 10:56:19 +0800

    2018年1月2日 10:55:31

--- ParentLibrary/src/main/java/com/parent/library/utils/LibStringUtil.java
+++ ParentLibrary/src/main/java/com/parent/library/utils/LibStringUtil.java
@@ -1,9 +1,11 @@
 package com.parent.library.utils;
 
+import com.sinothk.android.utils.OStringUtil;
+
 /**
  * Created by 梁玉涛 on 2018/1/2.
  */
 
-public class LibStringUtil {
+public class LibStringUtil extends OStringUtil{
 
 }
